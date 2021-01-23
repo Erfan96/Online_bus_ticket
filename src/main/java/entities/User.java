9ad8961d216478f88fb36524beb5a437025a9e31
efsam <E.salimi75@ymail.com>
2021-01-23 11:26:40 +0330

    commit User.java in secondary

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,26 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "client_user")
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}

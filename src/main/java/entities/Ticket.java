5d51ce1749307b97a93eeb461e89a9c0a360744b
efsam <E.salimi75@ymail.com>
2021-01-23 19:53:16 +0330

    commit Ticket.java in secondary

---
+++ src/main/java/entities/Ticket.java
@@ -0,0 +1,35 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ticket")
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "passenger_name")
+    private String passengerName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", nullable = false)
+    private Gender gender;
+
+    @ManyToOne
+    @JoinColumn(name = "trave_id", nullable = false)
+    private Travel travel;
+
+    @Column(name = "ticket_id", nullable = false)
+    private String ticketId;
+}

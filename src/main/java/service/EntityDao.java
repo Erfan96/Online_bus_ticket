93e4d8f3a4b6b9a0a92aa1c2c22dee5e78c6090f
efsam <E.salimi75@ymail.com>
2021-01-23 20:21:54 +0330

    commit EntityDao.java in secondary

---
+++ src/main/java/service/EntityDao.java
@@ -0,0 +1,29 @@
+package service;
+
+import javax.persistence.EntityManager;
+
+public abstract class EntityDao<T, U> {
+    protected EntityManager entityManager;
+
+    public EntityDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void save(T entity) {
+        entityManager.persist(entity);
+    }
+
+    public void update(T entity) {
+        entityManager.merge(entity);
+    }
+
+    public void delete(T entity) {
+        entityManager.remove(entity);
+    }
+
+    public T load(U id) {
+       return entityManager.find(getEntityClass(), id);
+    }
+
+    public abstract Class<T> getEntityClass();
+}

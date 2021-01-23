39317e9ba3910d7cc41f72c0ad9eddf9330290c5
efsam <E.salimi75@ymail.com>
2021-01-23 19:53:06 +0330

    commit Travel.java in secondary

---
+++ src/main/java/entities/Travel.java
@@ -0,0 +1,38 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "travel")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Travel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "origin", nullable = false)
+    private City origin;
+
+    @ManyToOne
+    @JoinColumn(name = "destination", nullable = false)
+    private City destination;
+
+    @Column(name = "departure_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date departureDate;
+
+    @Column(name = "departure_time", nullable = false)
+    private String departureTime;
+
+    @Column(name = "travel_id", nullable = false)
+    private String travelId;
+}

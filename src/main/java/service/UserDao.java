14057ef320ed0258d4352685b7fc7d81ff938f5b
efsam <E.salimi75@ymail.com>
2021-01-24 13:22:41 +0330

    commit UserDao.java in secondary

--- src/main/java/service/UserDao.java
+++ src/main/java/service/UserDao.java
@@ -1,7 +1,14 @@
 package service;
 
 import entities.User;
+import util.JpaUtil;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 public class UserDao extends EntityDao<User, Integer>{
 
@@ -12,5 +19,27 @@ public class UserDao extends EntityDao<User, Integer>{
     @Override
     public Class<User> getEntityClass() {
         return User.class;
+    }
+
+    public boolean checkUser(String uName, String pass) {
+        try {
+
+            CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+            CriteriaQuery<User> criteria = cb.createQuery(User.class);
+            Root<User> fromUser = criteria.from(User.class);
+
+            Predicate predicate = cb.and(
+                    cb.equal(fromUser.get("userName"), uName),
+                    cb.equal(fromUser.get("password"), pass)
+            );
+
+           criteria.select(fromUser).where(predicate);
+           entityManager.createQuery(criteria).getSingleResult();
+
+        } catch (NoResultException e) {
+            return false;
+        }
+
+        return true;
     }
 }

ad4231c180ec93f4324a0a4455afa81f43adf71a
efsam <E.salimi75@ymail.com>
2021-01-24 13:22:27 +0330

    commit LoginFilter.java in secondary

--- src/main/java/servlet/LoginFilter.java
+++ src/main/java/servlet/LoginFilter.java
@@ -2,6 +2,9 @@ package servlet;
 
 
 import service.UserDao;
+import util.JpaUtil;
+
+import javax.persistence.EntityManager;
 import javax.servlet.*;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,18 +20,25 @@ public class LoginFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         servletResponse.setContentType("text/html");
 
+        EntityManager entityManager = JpaUtil.getEntityManagerFactory().createEntityManager();
+
         try(PrintWriter out = servletResponse.getWriter()) {
 
+            UserDao userDao = new UserDao(entityManager);
             String username = servletRequest.getParameter("userName");
             String password = servletRequest.getParameter("password");
 
-            if (UserDao.checkUser(username, password)) {
+            if (userDao.checkUser(username, password)) {
                 filterChain.doFilter(servletRequest, servletResponse);
             }
             else {
                 out.println("Username or password is not correct!");
                 servletRequest.getRequestDispatcher("login.jsp").include(servletRequest, servletResponse);
             }
+        }
+        finally {
+            entityManager.close();
+            JpaUtil.Shutdown();
         }
     }
 

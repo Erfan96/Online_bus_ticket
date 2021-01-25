c649b407537ba254000b4145dd4304ecc16a0f76
efsam <E.salimi75@ymail.com>
2021-01-25 11:11:36 +0330

    commit CityDao.java in secondary

--- src/main/java/service/CityDao.java
+++ src/main/java/service/CityDao.java
@@ -2,6 +2,11 @@ package service;
 
 import entities.City;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class CityDao extends EntityDao<City, Integer>{
 
@@ -12,5 +17,15 @@ public class CityDao extends EntityDao<City, Integer>{
     @Override
     public Class<City> getEntityClass() {
         return City.class;
+    }
+
+    public List<City> getAllCities() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<City> criteria = cb.createQuery(City.class);
+        Root<City> fromCity = criteria.from(City.class);
+
+        criteria.select(fromCity).orderBy(cb.asc(fromCity.get("name")));
+        TypedQuery<City> query = entityManager.createQuery(criteria);
+        return query.getResultList();
     }
 }

c3b2d3ed7878bf8bad89bfbead3040bf2b1b4e5a
efsam <E.salimi75@ymail.com>
2021-01-25 11:17:13 +0330

    commit LoginServlet.java in secondary

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -1,21 +1,28 @@
 package servlet;
 
+import entities.City;
+import service.CityDao;
+import util.JpaUtil;
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
 
 public class LoginServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
 
-        try (PrintWriter out = resp.getWriter()){
-            out.println("Hello");
-        }
+        EntityManager entityManager = JpaUtil.getEntityManagerFactory().createEntityManager();
+        CityDao cityDao = new CityDao(entityManager);
+
+        List<City> cityList = cityDao.getAllCities();
+        req.setAttribute("list", cityList);
+        req.getRequestDispatcher("search.jsp").forward(req, resp);
     }
 }

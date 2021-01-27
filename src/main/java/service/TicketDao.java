5c361078565076b77fe0302e6b24896567c67c9d
efsam <E.salimi75@ymail.com>
2021-01-27 09:31:23 +0330

    commit TicketDao.java in secondary

--- src/main/java/service/TicketDao.java
+++ src/main/java/service/TicketDao.java
@@ -9,6 +9,7 @@ import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -46,6 +47,22 @@ public class TicketDao extends EntityDao<Ticket, Integer>{
 
         criteria.multiselect(fromTicket.get("ticketId"), fromTicket.get("travel").get("departureDate"))
                 .where(cb.equal(fromTicket.get("user"), user));
+
+        TypedQuery<Tuple> query = entityManager.createQuery(criteria);
+        return query.getResultList();
+    }
+
+    public List<Tuple> getDetailsOfTicket(String ticketId) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> criteria = cb.createTupleQuery();
+        Root<Ticket> fromTicket = criteria.from(Ticket.class);
+
+        Join<Ticket, Travel> ticketTravelJoin = fromTicket.join("travel");
+        criteria.multiselect(fromTicket.get("ticketId"), fromTicket.get("passengerName"),
+                fromTicket.get("gender"),ticketTravelJoin.get("origin").get("name")
+                , ticketTravelJoin.get("destination").get("name"), fromTicket.get("travel").get("departureDate"),
+                fromTicket.get("travel").get("departureTime"), fromTicket.get("travel").get("travelId"))
+                .where(cb.equal(fromTicket.get("ticketId"), ticketId));
 
         TypedQuery<Tuple> query = entityManager.createQuery(criteria);
         return query.getResultList();

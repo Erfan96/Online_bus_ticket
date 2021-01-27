042f0c70f7c916fbd17e745c4169ddbc7b34b4a3
efsam <E.salimi75@ymail.com>
2021-01-27 07:59:50 +0330

    commit LoginServlet.java in secondary

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -2,13 +2,13 @@ package servlet;
 
 
 import entities.City;
+import entities.User;
 import service.CityDao;
+import service.UserDao;
 import util.JpaUtil;
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.util.List;
 
@@ -21,6 +21,24 @@ public class LoginServlet extends HttpServlet {
 
         EntityManager entityManager = JpaUtil.getEntityManagerFactory().createEntityManager();
         CityDao cityDao = new CityDao(entityManager);
+
+        String userName = null;
+        String pass = null;
+
+        Cookie[] cookie = req.getCookies();
+        for (Cookie cookies : cookie) {
+            if (cookies.getName().equals("user")) {
+                userName = cookies.getValue();
+            }
+            else if(cookies.getName().equals("pass")) {
+                pass = cookies.getValue();
+            }
+        }
+
+        UserDao userDao = new UserDao(entityManager);
+        User user = userDao.getUserWithUsernameAndPassword(userName, pass);
+        HttpSession session = req.getSession();
+        session.setAttribute("use", user);
 
         List<City> cityList = cityDao.getAllCities();
         req.setAttribute("list", cityList);
